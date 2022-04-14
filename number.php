30792deed4a83d454410ad951760ce30dcc375cc
HyangYoung <hyangyoungs@gmail.com>
2022-04-14 10:45:14 -0500

    numbers&string

--- number.php
+++ number.php
@@ -1,3 +1,22 @@
-<?php
-phpinfo();
-?>
+<!doctype html>
+<html>
+<body>
+<h1>Number & Operator</h1>
+    <h3>1+1</h3>
+    <?php
+    echo 1+1;
+    ?>
+    <h3>2-1</h3>
+    <?php
+    echo 2-1;
+    ?>
+    <h3>2*2</h3>
+    <?php
+    echo 2*2;
+    ?>
+    <h3>4/2</h3>
+    <?php
+    echo 4/2;
+    ?>
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<body>
+<h1>String & String Operator</h1>
+<?php
+echo "Hello \"w\" world";
+?>
+<h2>concatenation operator</h2>
+<?php
+echo "Hello "."world";
+?>
+<h2>string length function</h2>
+<?php
+echo strlen("Hello world");
+?>
+</body>
+</html>
+
