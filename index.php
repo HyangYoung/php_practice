5434028e5aaa568f65788d8c7ef2a2790523eb1f
HyangYoung <hyangyoungs@gmail.com>
2022-04-15 23:43:08 -0500

    hyperlink

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <!doctype html>
 <html>
+<head>
 <body>
+<h1>WEB</h1>
+<ol>
+    <li><a href="index.php?id=HTML">HTML</a></li>
+    <li><a href="index.php?id=CSS">CSS</a></li>
+    <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+</ol>
 <?php
-echo date('Y-m-d H:i:s');
+echo $_GET['id'];
 ?>
 </body>
+</head>
 </html>
