64cbf86157762009fafd92862273038f01e8fceb
HyangYoung <hyangyoungs@gmail.com>
2022-04-15 23:55:01 -0500

    functions

--- function.php
+++ function.php
@@ -7,7 +7,9 @@
 <body>
 <h1>function</h1>
 <?php
-$str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+$str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 echo $str;
 ?>
 <h2>strlen()</h2>
