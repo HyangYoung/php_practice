cb62c5489c880877de846923f0419d9df9f65145
HyangYoung <hyangyoungs@gmail.com>
2022-04-18 21:31:35 -0500

    function, Get

--- function.php
+++ function.php
@@ -12,11 +12,11 @@ $str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 echo $str;
 ?>
-<h2>strlen()</h2>
+<h2>strlen()</h2> //length of string (str -> int)
 <?php
 echo strlen($str);
 ?>
-<h2>nl2br</h2>
+<h2>nl2br</h2> //new line to Br
 <?php
 echo nl2br($str);
 ?>
