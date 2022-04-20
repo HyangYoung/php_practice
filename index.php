875400ec79e3f321e07fa13ce2cfe320baa0508f
HyangYoung <hyangyoungs@gmail.com>
2022-04-20 10:53:54 -0500

    while loop

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
 </head>
 <body>
 <h1><a href="index.php">WEB</a></h1>
+//data 디렉토리에 있는 파일을 가져오고, 파일의 목록 하나하나를 li와 a태그를 이용해서 글목록을 만드세요.
+//site:php.net, php loop statements
+<?php
+    while (expr)
+        statment
+?>
+
+
 <ol>
     <li><a href="index.php?id=HTML">HTML</a></li>
     <li><a href="index.php?id=CSS">CSS</a></li>
     <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    <li><a href="index.php?id=PHP">PHP</a></li>
 </ol>
 <h2>
     <?php
---
+++ loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>title</title>
+</head>
+
+<body>
+    <h1>while</h1>
+    <?php
+    echo '1<br>';
+    $i = 0;
+    while($i < 3){
+        echo '2<br>';
+        $i= $i + 1;
+    }
+    echo '3<br>';
+    ?>
+</body>
+</html>
+
+
