aa9a78aeb930d265cad583ba5b1c976f7d4a0afe
HyangYoung <hyangyoungs@gmail.com>
2022-04-19 22:02:48 -0500

    if/else

---
+++ conditional.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>title</title>
+
+    <link rel="stylesheet" media="screen" href="css 주소">
+</head>
+
+<body>
+    <h1>Conditional</h1>
+    <h2>if</h2>
+    <?php
+    echo '1<br>';
+    if (false) {
+        echo '2-1<br>';
+    } else {
+        echo '2-2<br>';
+    }
+    echo '3<br>';
+    ?>
+</body>
+</html>
+
+
