f7446c4b879aafdf7715a6abb8f8fbe56a9c0485
HyangYoung <hyangyoungs@gmail.com>
2022-04-19 21:51:57 -0500

    Comparison Operators

---
+++ comparison.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>title</title>
+
+</head>
+
+<body>
+    <h1>Comparison Operators &amp;
+        Boolean data type
+    </h1>
+    <h2>1==1</h2>
+    <?php
+    var_dump(1==1); //데이터타입을 함께 표시해 줌
+    ?>
+    <h2>1>=1</h2>
+    <?php
+    var_dump(1>=1);
+    ?>
+    <h2>1>1</h2>
+    <?php
+    var_dump(1>1);
+    ?>
+</body>
+</html>
+
+
