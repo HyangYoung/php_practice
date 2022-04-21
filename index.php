4fcee695b14bf8a6565e2b8e894141f8aace52f8
HyangYoung <hyangyoungs@gmail.com>
2022-04-21 11:46:52 -0500

    Use while/if to create linke

--- index.php
+++ index.php
@@ -6,15 +6,25 @@
 </head>
 <body>
 <h1><a href="index.php">WEB</a></h1>
-//data 디렉토리에 있는 파일을 가져오고, 파일의 목록 하나하나를 li와 a태그를 이용해서 글목록을 만드세요.
-//site:php.net, php loop statements
+<!--data 디렉토리에 있는 파일을 가져오고, 파일의 목록 하나하나를 li와 a태그를 이용해서 글목록을 만드세요.-->
+<!--site:php.net, php loop statements-->
 <ol>
     <?php
         $list = scandir('./data');
-        var_dump($list);
+
+        $i = 2;
+        while ($i < count($list)){
+            if($list[$i] != '.') {
+                if ($list[$i] != '..') {
+                    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                }
+            }
+            $i = $i + 1;
+        }
     ?>
 </ol>
 
+<h2>
     <?php
     if(isset($_GET['id'])){
         echo $_GET['id'];
@@ -23,6 +33,7 @@
     }
     ?>
 </h2>
+
 <?php
 if(isset($_GET['id'])){
     echo file_get_contents("data/".$_GET['id']);
