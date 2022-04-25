293e79931b31515bdd1ed70927364cba3a088036
HyangYoung <hyangyoungs@gmail.com>
2022-04-24 22:47:01 -0500

    return function

--- function2.php
+++ function2.php
@@ -31,5 +31,13 @@
     sum(4,6);
     ?>
 
+    <?php
+    function sum2($left, $right){
+        return $left+$right;
+    }
+    print(sum2(2,4));
+    file_put_contents('result.txt', sum2(2,4));
+    ?>
+
 </body>
 </html>
